fbd59936776d6867da0e7ac5c124811288422920
danielFarfan18 <119150840+danielFarfan18@users.noreply.github.com>
2024-03-14 12:34:33 -0600

    Coomment code from api,py and retro_plan.py files

--- retro_star/api.py
+++ retro_star/api.py
@@ -1,35 +1,58 @@
+"""
+This script defines the RSPlanner class for retrosynthetic planning in chemistry. 
+Retrosynthetic planning is a process where you start with a target molecule and plan backwards 
+to find simpler starting molecules and the reactions needed to synthesize the target. 
+
+The RSPlanner class uses a Multi-Layer Perceptron (MLP) model for one-step retrosynthesis prediction 
+and optionally a ValueMLP model as a value function to guide the search. 
+
+The plan method of the RSPlanner class performs the retrosynthetic planning for a given target molecule 
+and returns the result. 
+
+If the script is run directly, it creates an instance of RSPlanner and calls the plan method for 
+three different target molecules.
+"""
 import torch
 import logging
 import time
+# Importing necessary modules from the project
 from retro_star.common import prepare_starting_molecules, prepare_mlp, \
     prepare_molstar_planner, smiles_to_fp
 from retro_star.model import ValueMLP
 from retro_star.utils import setup_logger
 
 import os
+# Getting the directory path of the current file
 dirpath = os.path.dirname(os.path.abspath(__file__))
 
+# Defining the RSPlanner class
 class RSPlanner:
+    # The constructor for the RSPlanner class
     def __init__(self,
-                 gpu=-1,
-                 expansion_topk=50,
-                 iterations=500,
-                 use_value_fn=False,
-                 starting_molecules=dirpath+'/dataset/origin_dict.csv',
-                 mlp_templates=dirpath+'/one_step_model/template_rules_1.dat',
-                 mlp_model_dump=dirpath+'/one_step_model/saved_rollout_state_1_2048.ckpt',
-                 save_folder=dirpath+'/saved_models',
-                 value_model='best_epoch_final_4.pt',
-                 fp_dim=2048,
-                 viz=False,
-                 viz_dir='viz'):
+                 gpu=-1,  # The GPU device number to use. If -1, CPU is used.
+                 expansion_topk=50,  # The number of top molecules to consider for expansion
+                 iterations=500,  # The number of iterations to perform
+                 use_value_fn=False,  # Whether to use a value function to guide the search
+                 starting_molecules=dirpath+'/dataset/origin_dict.csv',  # The path to the file containing the starting molecules
+                 mlp_templates=dirpath+'/one_step_model/template_rules_1.dat',  # The path to the file containing the MLP templates
+                 mlp_model_dump=dirpath+'/one_step_model/saved_rollout_state_1_2048.ckpt',  # The path to the file containing the MLP model dump
+                 save_folder=dirpath+'/saved_models',  # The folder to save the models
+                 value_model='best_epoch_final_4.pt',  # The name of the value model file
+                 fp_dim=2048,  # The dimension of the fingerprint
+                 viz=False,  # Whether to visualize the planning process
+                 viz_dir='viz'):  # The directory to save the visualizations
 
+        # Setting up the logger
         setup_logger()
+        # Setting the device for torch
         device = torch.device('cuda:%d' % gpu if gpu >= 0 else 'cpu')
+        # Preparing the starting molecules
         starting_mols = prepare_starting_molecules(starting_molecules)
 
+        # Preparing the MLP
         one_step = prepare_mlp(mlp_templates, mlp_model_dump)
 
+        # If use_value_fn is True, load the model and define the value function
         if use_value_fn:
             model = ValueMLP(
                 n_layers=1,
@@ -49,8 +72,10 @@ class RSPlanner:
                 v = model(fp).item()
                 return v
         else:
+            # If use_value_fn is False, define a dummy value function
             value_fn = lambda x: 0.
 
+        # Preparing the planner
         self.plan_handle = prepare_molstar_planner(
             one_step=one_step,
             value_fn=value_fn,
@@ -61,27 +86,31 @@ class RSPlanner:
             viz_dir=viz_dir
         )
 
-    def plan(self, target_mol):
+    # The plan method for the RSPlanner class
+    def plan(self, target_mol):  # The target molecule to plan for
         t0 = time.time()
         succ, msg = self.plan_handle(target_mol)
 
+        # If the plan is successful, return the result
         if succ:
             result = {
-                'succ': succ,
-                'time': time.time() - t0,
-                'iter': msg[1],
-                'routes': msg[0].serialize(),
-                'route_cost': msg[0].total_cost,
-                'route_len': msg[0].length
+                'succ': succ,  # Whether the plan was successful
+                'time': time.time() - t0,  # The time taken for the plan
+                'iter': msg[1],  # The number of iterations performed
+                'routes': msg[0].serialize(),  # The routes found
+                'route_cost': msg[0].total_cost,  # The total cost of the route
+                'route_len': msg[0].length  # The length of the route
             }
             return result
 
         else:
+            # If the plan is not successful, log the message and return None
             logging.info('Synthesis path for %s not found. Please try increasing '
                          'the number of iterations.' % target_mol)
             return None
 
 
+# If the script is run directly, create an instance of RSPlanner and call the plan method
 if __name__ == '__main__':
     planner = RSPlanner(
         gpu=0,
@@ -98,4 +127,3 @@ if __name__ == '__main__':
 
     result = planner.plan('CC(C)c1ccc(-n2nc(O)c3c(=O)c4ccc(Cl)cc4[nH]c3c2=O)cc1')
     print(result)
-
--- retro_star/retro_plan.py
+++ retro_star/retro_plan.py
@@ -1,3 +1,7 @@
+"""This script is used for retrosynthetic planning using a method called Retro* (Retro-Star).
+Retrosynthetic planning is a process in chemistry where you start with a target molecule 
+and plan backwards to find simpler starting molecules and the reactions needed to synthesize the target.
+"""
 import numpy as np
 import torch
 import random
@@ -10,22 +14,27 @@ from retro_star.common import args, prepare_starting_molecules, prepare_mlp, \
 from retro_star.model import ValueMLP
 from retro_star.utils import setup_logger
 
-
 def retro_plan():
+    # Set up the device for computation (CPU or GPU).
     device = torch.device('cuda' if args.gpu >= 0 else 'cpu')
 
+    # Prepare the starting molecules.
     starting_mols = prepare_starting_molecules(args.starting_molecules)
 
+    # Load the test routes (retrosynthetic plans) from a file.
     routes = pickle.load(open(args.test_routes, 'rb'))
-    logging.info('%d routes extracted from %s loaded' % (len(routes),
-                                                         args.test_routes))
+    logging.info('%d routes extracted from %s loaded' % (len(routes), args.test_routes))
 
+    # Prepare the MLP (Multi-Layer Perceptron) model for one-step retrosynthesis prediction.
     one_step = prepare_mlp(args.mlp_templates, args.mlp_model_dump)
 
-    # create result folder
+    # Create a result folder if it doesn't exist.
     if not os.path.exists(args.result_folder):
         os.mkdir(args.result_folder)
 
+    # If use_value_fn is True, load a pre-trained ValueMLP model from a file and define a value function 
+    # that uses this model to predict the value of a molecule. 
+    # If use_value_fn is False, define a value function that always returns 0.
     if args.use_value_fn:
         model = ValueMLP(
             n_layers=args.n_layers,
@@ -47,6 +56,7 @@ def retro_plan():
     else:
         value_fn = lambda x: 0.
 
+    # Prepare the Retro* planner with the one-step MLP, the value function, and the starting molecules.
     plan_handle = prepare_molstar_planner(
         one_step=one_step,
         value_fn=value_fn,
@@ -57,6 +67,7 @@ def retro_plan():
         viz_dir=args.viz_dir
     )
 
+    # Initialize an empty dictionary to store the results.
     result = {
         'succ': [],
         'cumulated_time': [],
@@ -68,7 +79,8 @@ def retro_plan():
     num_targets = len(routes)
     t0 = time.time()
     for (i, route) in enumerate(routes):
-
+        # For each route in the test routes, use the Retro* planner to plan the synthesis of the target molecule. 
+        # Store the success status, cumulative time, number of iterations, planned route, route cost, and route length in the result dictionary.
         target_mol = route[0].split('>')[0]
         succ, msg = plan_handle(target_mol, i)
 
@@ -90,11 +102,14 @@ def retro_plan():
         logging.info('Succ: %d/%d/%d | avg time: %.2f s | avg iter: %.2f' %
                      (tot_succ, tot_num, num_targets, avg_time, avg_iter))
 
+    # After planning for all routes, save the result dictionary to a file.
     f = open(args.result_folder + '/plan.pkl', 'wb')
     pickle.dump(result, f)
     f.close()
 
 if __name__ == '__main__':
+    # If the script is run as the main program, set the random seed for numpy, torch, and the random module to ensure reproducibility, 
+    # set up a logger, and call the retro_plan function.
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     random.seed(args.seed)
